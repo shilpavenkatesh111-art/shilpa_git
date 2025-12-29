af1b7fa14dd8e3c22ce8a8a6ca6ae2a27aaf7d1c
Divya C L <Divya C L@DESKTOP-2PHPB8V>
2025-12-29 13:01:04 +0530

    new commit msg

--- src/test/java/test/TE_Divya.java
+++ src/test/java/test/TE_Divya.java
@@ -4,7 +4,8 @@ public class TE_Divya {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("script has been created");
+		System.out.println("Hi Geetha....how are you");
+		System.out.println("when will be the code review meeting");
 	}
 
 }
